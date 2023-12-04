0f9902432967aa402d7748b337465da5a2d8defe
Ivan Kaurov <crazyanimalcz@gamil.com>
2023-12-04 17:57:49 +0300

    Added fixes

--- main.py
+++ main.py
@@ -10,28 +10,29 @@ API_KEY = os.getenv('EXCHANGE_RATE_API_KEY')
 
 
 def function(message, currency):
-    if message.text == 'текущий':
-        url = f"https://api.apilayer.com/exchangerates_data/latest?base={currency}"
-        response = requests.get(url, headers={"apikey": API_KEY})
-        rate = response.json()['rates']['RUB']
-        bot.send_message(message.chat.id, f'Курс {currency} к рублю: {rate:.2f}')
-    else:
-        date_list = message.text.split('.')
-        formated_date = []
-        for i in reversed(range(len(date_list))):
-            formated_date.append(date_list[i])
-        user_date = '-'.join(formated_date)
-        url = f"https://api.apilayer.com/exchangerates_data/{user_date}?&base={currency}"
-        response = requests.get(url, headers={"apikey": API_KEY})
-        rate = response.json()['rates']['RUB']
-        year, month, day = user_date.split('-')
-        bot.send_message(message.chat.id, f'Курс {currency} к рублю: {rate:.2f} на момент {day}.{month}.{year}')
+        try:
+            if message.text.lower() == 'текущий':
+                url = f"https://api.apilayer.com/exchangerates_data/latest?base={currency}"
+                response = requests.get(url, headers={"apikey": API_KEY})
+                rate = response.json()['rates']['RUB']
+                bot.send_message(message.chat.id, f'Курс {currency} к рублю: {rate:.2f}')
+            else:
+                date_list = message.text.split('.')
+                formated_date = []
+                for i in reversed(range(len(date_list))):
+                    formated_date.append(date_list[i])
+                user_date = '-'.join(formated_date)
+                url = f"https://api.apilayer.com/exchangerates_data/{user_date}?&base={currency}"
+                response = requests.get(url, headers={"apikey": API_KEY})
+                rate = response.json()['rates']['RUB']
+                year, month, day = user_date.split('-')
+                bot.send_message(message.chat.id, f'Курс {currency} к рублю: {rate:.2f} на момент {day}.{month}.{year}')
+        except(KeyError):
+            bot.send_message(message.chat.id, 'Некорректная форма даты. Попробуйте снова')
 
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    # markup = types.ReplyKeyboardMarkup()
-    # markup.row(types.KeyboardButton('Начать'))
     bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!')
     bot.send_message(message.chat.id, 'Я - <b>бот</b>, который подсказывает курс валюты',
                      parse_mode='html')
@@ -52,7 +53,7 @@ def callback_data(call):
     currency = call.data
     bot.send_message(call.message.chat.id, 'Интересует текущий курс или за конкретную дату?')
     bot.send_message(call.message.chat.id,
-                     'Если <b>текущий</b> - напишите "текущий"\nЗа <b>конкретную дату</b> - укажите дату в формате ДД.ММ.ГГГГ',
+                     'Если <b>текущий</b> - напишите "текущий"\nЗа <b>конкретную дату</b> - укажите дату в формате ДД.ММ.ГГГГ\nПримечание: <b>до</b> 99г включительно',
                      parse_mode='html')
     bot.register_next_step_handler(call.message, function, currency)
 
